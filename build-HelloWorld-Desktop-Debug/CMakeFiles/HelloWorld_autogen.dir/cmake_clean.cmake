1febb96244b4cf6fdcbb53d5922a39dddd64378b
MrX-OvO <33060485+MrX-OvO@users.noreply.github.com>
2021-11-26 18:55:14 +0800

    Delete build-HelloWorld-Desktop-Debug directory

--- build-HelloWorld-Desktop-Debug/CMakeFiles/HelloWorld_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/HelloWorld_autogen"
-  "HelloWorld_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/HelloWorld_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
