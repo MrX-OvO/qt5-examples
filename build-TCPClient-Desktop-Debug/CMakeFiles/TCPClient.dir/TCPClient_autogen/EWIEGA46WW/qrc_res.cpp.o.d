4e73eaff6565fdfdbe62d77e8ea55024819c06b0
MrX-OvO <33060485+MrX-OvO@users.noreply.github.com>
2021-11-26 18:56:35 +0800

    Delete build-TCPClient-Desktop-Debug directory

--- build-TCPClient-Desktop-Debug/CMakeFiles/TCPClient.dir/TCPClient_autogen/EWIEGA46WW/qrc_res.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/TCPClient.dir/TCPClient_autogen/EWIEGA46WW/qrc_res.cpp.o: \
- /home/mrx_ovo/Programs/qt5/build-TCPClient-Desktop-Debug/TCPClient_autogen/EWIEGA46WW/qrc_res.cpp \
- /usr/include/stdc-predef.h
