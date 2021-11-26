a49f122247dcae707b6d0a36fa7ebca10e69bc60
MrX-OvO <33060485+MrX-OvO@users.noreply.github.com>
2021-11-26 18:56:49 +0800

    Delete build-TCPServer-Desktop-Debug directory

--- build-TCPServer-Desktop-Debug/CMakeFiles/TCPServer.dir/TCPServer_autogen/EWIEGA46WW/qrc_res.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/TCPServer.dir/TCPServer_autogen/EWIEGA46WW/qrc_res.cpp.o: \
- /home/mrx_ovo/Programs/qt5/build-TCPServer-Desktop-Debug/TCPServer_autogen/EWIEGA46WW/qrc_res.cpp \
- /usr/include/stdc-predef.h
